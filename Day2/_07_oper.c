6c0fa6e522011f4eea3b1d646096107ceb5cfdc6
zcbmxvn27 <baoprots1@gmail.com>
2022-07-24 17:34:20 +0900

    재구성

--- Day2/_07_oper.c
+++ Day2/_07_oper.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 void main() {
-	int num1 = 0, score1 = 0;
-	int num2 = 0, score2 = 0;
-	int num3 = 0, score3 = 0;
-	int num4 = 0, score4 = 0;
-	int name2 = 0, result = 0;
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	int num4 = 0;
+	int name2 = 0;
 	char name1[50];
 	// 학습지 사칙연산
 	//이름을 입력하세요 (출력)
@@ -21,22 +21,18 @@ void main() {
 	printf("1 + 1 = ");
 	scanf("%d", &num1);
 	(num1 == 2) ? printf("정답입니다.\n") : printf("오답입니다.\n");
-	(num1 == 2) ? score1 = 1 : score1 = 0;
 	// 2 - 1
 	printf("2 - 1 = ");
 	scanf("%d", &num2);
 	(num2 == 1) ? printf("정답입니다.\n") : printf("오답입니다.\n");
-	(num2 == 1) ? score2 = 1 : score2 = 0;
 	// 3*3
 	printf("3 × 3 = ");
 	scanf("%d", &num3);
 	(num3 == 9) ? printf("정답입니다.\n") : printf("오답입니다.\n");
-	(num3 == 9) ? score3 = 1 : score3 = 0;
 	//10/2
 	printf("10 ÷ 2 = ");
 	scanf("%d", &num4);
 	(num4 == 5) ? printf("정답입니다.\n") : printf("오답입니다.\n");
-	(num4 == 5) ? score4 = 1 : score4 = 0;
 
 	printf("-------------------------------------------------------------------------------------------------------\n");
 	printf("이름:%s | 학번:%d \n", name1, name2);
